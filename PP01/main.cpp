42ff7486bdb87c99863fbc2adf09f240e41562c1
준석 이 <skysk1231@yuhan.ac.kr>
2022-03-15 19:45:21 +0900

    virtual 함수 예제2번

--- PP01/main.cpp
+++ PP01/main.cpp
@@ -1,23 +1,35 @@
 ﻿#include <iostream>
 #include <string>
 
-using namespace std;// namespace 같은 함수 이름 구분 가능 
+using namespace std;
 
 class Parent 
 {
 public:
-    void print() // virtual void print() 로 적을시에 32번 줄의 출력값이 이곳은 Child입니다 로 변경 
+     void fun1() 
     {
-        cout << "이곳은 Parent입니다.\n";
+        cout << "이곳은 Parent클래스의 fun1함수입니다.\n";
+    }
+    virtual void fun2() // virtual을 통해 가상 함수로 지정
+    {
+        cout << "이곳은 Parent클래스의 fun2함수입니다.\n";
+    }
+    void fun3()
+    {
+        cout << "이곳은 Parent클래스의 fun3함수입니다.\n";
     }
 };
 
 class Child : public Parent
 {
 public:
-    void print()
+    void fun1()
     {
-        cout << "이곳은 Child입니다.\n";
+        cout << "이곳은 Child 클래스의 fun1함수입니다.\n";
+    }
+    virtual void fun2()
+    {
+        cout << "이곳은 Child 클래스의 fun2함수입니다.\n";
     }
 };
 
@@ -27,10 +39,18 @@ int main()
     Parent* p = new Parent; 
     Child* c = new Child;
 
-    p->print(); //Parent클레스의 print 함수를 호출 -> 결과값이 이곳은 Parent입니다 출력
-    p = c;// p의 주소값을 c의 주소값으로 변경
-    p->print(); //Child클래스의 print 함수를 호출 But p의 주소값이 고정되어 있어서 결과값이 이곳은 Parent입니다 출력
+    p->fun1(); 
+    c->fun1();
+    p->fun2();
+    c->fun2();
+    p->fun3();
+    c->fun3(); // Child클래스의 해당하는 fun3함수가 없기 때문에 부모 클래스인 Parent 클래스에서 fun3함수의 결과를 출력
+    p = c;
 
-    // 해결법 p의 주소값이 고정되는 것을 막기 위해서 virtual함수를 사용해서 동적 정수로 함수를 선언
+    cout << "\n\n";
+    p->fun1();// Parent 클래스의 fun1고정 함수이기 때문에 Parent클래스의 fun1함수 주소값이 고정 41번째 줄과 같은값 출력
+    p->fun2();// p의 주소값이 c의 주소값이 되었기 때문에 Child클래스의 fun2함수값 출력
+    p->fun3();// Child클래스의 해당하는 fun3함수가 없기 때문에 부모 클래스인 Parent 클래스에서 fun3함수의 결과를 출력
+   
     return 0;
 }
