49df0914e23a7ac8aa80aafaedf24e77ef6db0e8
준석 이 <skysk1231@yuhan.ac.kr>
2022-03-15 14:54:50 +0900

    class player 생성자 + 소멸자

--- PP01/main.cpp
+++ PP01/main.cpp
@@ -1,7 +1,7 @@
 ﻿#include <iostream>
 #include <string>
 
-using namespace std;// 밑에 std 제거 가능
+using namespace std;// namespace 같은 함수 이름 구분 가능 
 
 class Player //접근자 없을시 private
 {
@@ -31,12 +31,33 @@ public:
 private:
 };
 
+class Player3//접근자 없을시 private
+{
+public:
+    Player3() // 생성자
+    {
+        cout << "주인공3 생성되었습니다! 아 맑은 공기 아름다운 세상" << endl; // 메모리 아끼기
+    }
+    ~Player3()// 소멸자
+    {
+        cout << "앗.. 안돼....." << endl; //성능 속도가 빠름
+    }
+private:
+};
+
+void a()
+{
+    Player3 pl3;
+}
+
 int main()
 {   
-    Player2 pl2;
+    a();
 
-    Player *player = new Player(); //메모리가 만들어졌지만 소멸하지 않음
-    delete(player); // 소멸 시킴
+    Player2 pl2; // Player보다 먼저 생성되고 나중에 소멸됨 // 정적 할당 // 스택
+   
+    Player *player = new Player(); //메모리가 만들어졌지만 소멸하지 않음 // 동적 할당 // 소멸시키시기 전까지 소멸 X //힙
+    delete(player); // 소멸 시킴 
 
     return 0;
 }
