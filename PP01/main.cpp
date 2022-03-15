8a03a2690bf25ecf5cd7acd2c4542dc45b4eb32f
준석 이 <skysk1231@yuhan.ac.kr>
2022-03-15 19:31:03 +0900

    virtual 함수 예제 1번

--- PP01/main.cpp
+++ PP01/main.cpp
@@ -2,104 +2,35 @@
 #include <string>
 
 using namespace std;// namespace 같은 함수 이름 구분 가능 
-bool is_game_running = false; //글로벌 변수_다른 파일에서 접근 가능
 
-class GameObject
-{
-public :
-    GameObject() 
-    {
-        cout << "게임오브젝트 생성\n";
-    }
-    virtual ~GameObject()
-    {
-        cout << "게임오브젝트 소멸\n";
-    }
-
-    virtual void Start() // 기본형이 지정되어 있다고 가정
-    {
-        cout << "게임오브젝트 스타트\n";
-
-    }
-    virtual void Update() // 기본형이 지정되어 있다고 가정
-    {
-        cout << "게임오브젝트 업데이트\n";
-
-    }
-};
-
-class Player : public GameObject
+class Parent 
 {
 public:
-    int HP = 100;
-    Player()
+    void print() // virtual void print() 로 적을시에 32번 줄의 출력값이 이곳은 Child입니다 로 변경 
     {
-        cout << "플레이어 생성\n";
-    }
-    ~Player()
-    {
-        cout << "플레이어 소멸\n";
-    }
-    void Start()
-    {
-        cout << "플레이어의 HP는" << HP << "입니다\n";
-    }
-    void Update()
-    {
-        cout << "플레이어 업데이트\n";
+        cout << "이곳은 Parent입니다.\n";
     }
 };
 
-class Enemy : public GameObject
+class Child : public Parent
 {
 public:
-    int HP = 100;
-    Enemy()
+    void print()
     {
-        cout << "적 생성\n";
-    }
-    ~Enemy()
-    {
-        cout << "적 소멸\n";
-    }
-    void Start()
-    {
-        cout << "적의 HP는" << HP <<"입니다\n";
-    }
-    void Update()
-    {
-        cout << "적 업데이트\n";
+        cout << "이곳은 Child입니다.\n";
     }
 };
+
 
 int main()
 {   
-    static bool is_game_running = true;  // 스태틱 변수_다른 파일에서 접근 불가능
+    Parent* p = new Parent; 
+    Child* c = new Child;
 
-    GameObject* p = new Player();
-    GameObject* e = new Enemy();
+    p->print(); //Parent클레스의 print 함수를 호출 -> 결과값이 이곳은 Parent입니다 출력
+    p = c;// p의 주소값을 c의 주소값으로 변경
+    p->print(); //Child클래스의 print 함수를 호출 But p의 주소값이 고정되어 있어서 결과값이 이곳은 Parent입니다 출력
 
-
-    p->Start(); // 동적 할당을 하게 되면 화살표 모양으로 표기
-    e->Start();
-    while (is_game_running)
-    {
-        p->Update();
-
-        e->Update();
-        cout << "입력해주세요 (1:게임 종료 , 2:게임 계속): ";
-        int input;
-        cin >> input;
-        if (input == 1)
-            is_game_running = false;
-        else if (input == 2)
-            is_game_running = true;
-        else
-            cout << "다시 입력해주세요\n";
-    };
-
-    delete(p); // 메모리 누수 생김 player가 생성 GameDbject만 제거 
-    delete(e);
-
+    // 해결법 p의 주소값이 고정되는 것을 막기 위해서 virtual함수를 사용해서 동적 정수로 함수를 선언
     return 0;
 }
